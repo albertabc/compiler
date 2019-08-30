dbb47be2256df604eac550d69e672b359bf0d32f
Zhang, Shouheng <zhangshouhengdtc@163.com>
2019-08-30 22:36:31 +0800

    Add math expression support
    1. Refine code
    2. Only support simple math expression.
    
    Example:
    2+3*5
    
    Signed-off-by: Zhang, Shouheng <zhangshouhengdtc@163.com>

--- main.cpp
+++ main.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 
 
 enum Dfstate {
-    Initial,
-    Int_I,
-    Int_N,
-    Int,
-    Indentifier,
-    IntLiteral,
+    INITIAL,
+    INT_I,
+    INT_N,
+    INT,
+    ID,
+    INT_LITERAL,
+    OPERAOTR,
     EQ,
     GT,
     GE,
 };
 
 enum TokenType {
-    Identifier_Type,
-    IntLiteral_Type,
-    Int_Type,
-    Eq_Type,
-    GT_Type,
-    GE_Type,
+    ID_TYPE,
+    INT_LITERAL_TYPE,
+    INT_TYPE,
+    EQ_TYPE,
+    GT_TYPE,
+    GE_TYPE,
+    PLUS_TYPE,
+    MINUS_TYPE,
+    STAR_TYPE,
+    SLASH_TYPE,
     None_Type,
 };
 
@@ -36,20 +42,30 @@ static void initToken(const char c, Dfstate& state, std::vector<Token>& res);
 static void handleState(const char c, Dfstate& state, std::vector<Token>& res);
 static std::string type2str(const TokenType& type);
 
+static std::unordered_map<char, TokenType> tokenMap = {{'+', PLUS_TYPE}, {'-', MINUS_TYPE}, {'*', STAR_TYPE}, {'/', SLASH_TYPE}};
+
 static std::string type2str(const TokenType& type) {
     switch (type) {
-        case Identifier_Type:
+        case ID_TYPE:
             return "Identifier";
-        case IntLiteral_Type:
+        case INT_LITERAL_TYPE:
             return "IntLiteral";
-        case Eq_Type:
+        case EQ_TYPE:
             return "ASSIGN";
-        case GT_Type:
+        case GT_TYPE:
             return "GT";
-        case GE_Type:
+        case GE_TYPE:
             return "GE";
-        case Int_Type:
+        case INT_TYPE:
             return "Int";
+        case PLUS_TYPE:
+            return "Plus";
+        case MINUS_TYPE:
+            return "Minus";
+        case STAR_TYPE:
+            return "Star";
+        case SLASH_TYPE:
+            return "Slash";
         default:
             return "None";
     }
@@ -82,25 +98,28 @@ static void handle(const std::string& src, const Dfstate& initialState, std::vec
 }
 
 static void initToken(const char c, Dfstate& state, std::vector<Token>& res) {
-    if (state != Dfstate::Initial)
+    if (state != INITIAL)
         return;
 
     Token token;
     if (c == 'i') {
-        state = Dfstate::Int_I;
-        token.type = TokenType::Identifier_Type;
+        state = INT_I;
+        token.type = ID_TYPE;
     } else if (isAlpha(c) || c == '_') {
-        state = Dfstate::Indentifier;
-        token.type = TokenType::Identifier_Type;
+        state = ID;
+        token.type = ID_TYPE;
     } else if (isDigital(c)) {
-        state = Dfstate::IntLiteral;
-        token.type = TokenType::IntLiteral_Type;
+        state = INT_LITERAL;
+        token.type = INT_LITERAL_TYPE;
     } else if (c == '>') {
-        state = Dfstate::GT;
-        token.type = TokenType::GT_Type;
+        state = GT;
+        token.type = GT_TYPE;
     } else if (c == '=') {
-        state = Dfstate::EQ;
-        token.type = TokenType::Eq_Type;
+        state = EQ;
+        token.type = EQ_TYPE;
+    } else if (c == '+' || c == '-' || c == '*' || c == '/') {
+        state = OPERAOTR;
+        token.type = tokenMap[c];
     } else {
         return;
     }
@@ -111,66 +130,65 @@ static void initToken(const char c, Dfstate& state, std::vector<Token>& res) {
 
 static void handleState(const char c, Dfstate& state, std::vector<Token>& res) {
     switch (state) {
-        case Dfstate::Initial:
+        case INITIAL:
+        case EQ:
+        case OPERAOTR:
+            state = INITIAL;
             initToken(c, state, res);
             break;
-        case Dfstate::Int_I:
+        case INT_I:
             if (c == 'n') {
-                state = Dfstate::Int_N;
+                state = INT_N;
             } else {
-                state = Dfstate::Indentifier;
+                state = ID;
             }
             res.back().val += c;
             break;
-        case Dfstate::Int_N:
+        case INT_N:
             if (c == 't') {
-                state = Dfstate::Int;
+                state = INT;
             } else {
-                state = Dfstate::Indentifier;
+                state = ID;
             }
             res.back().val += c;
             break;
-        case Dfstate::Int:
+        case INT:
             if (isBlank(c)) {
-                res.back().type = TokenType::Int_Type;
-                state = Dfstate::Initial;
+                res.back().type = INT_TYPE;
+                state = INITIAL;
                 initToken(c, state, res);
             } else {
-                state = Dfstate::Indentifier;
+                state = ID;
                 res.back().val += c;
             }
             break;
-        case Dfstate::Indentifier:
+        case ID:
             if (isAlpha(c) || isDigital(c) || c == '_') {
                 res.back().val += c;
             } else {
-                state = Dfstate::Initial;
+                state = INITIAL;
                 initToken(c, state, res);
             }
             break;
-        case Dfstate::EQ:
-            state = Dfstate::Initial;
-            initToken(c, state, res);
-            break;
-        case Dfstate::GT:
+        case GT:
             if (c == '=') {
-                state = Dfstate::GE;
-                res.back().type = TokenType::GE_Type;
+                state = GE;
+                res.back().type = GE_TYPE;
                 res.back().val += c;
             } else {
-                state = Dfstate::Initial;
+                state = INITIAL;
                 initToken(c, state, res);
             }
             break;
-        case Dfstate::GE:
-            state = Dfstate::Initial;
+        case GE:
+            state = INITIAL;
             initToken(c, state, res);
             break;
-        case Dfstate::IntLiteral:
+        case INT_LITERAL:
             if (isDigital(c)) {
                 res.back().val += c;
             } else {
-                state = Dfstate::Initial;
+                state = INITIAL;
                 initToken(c, state, res); 
             }
             break;
@@ -186,10 +204,14 @@ int main(int, char**) {
     std::string str1 = "int age = 40";
     std::string str2 = "intA_0=50";
     std::string str3 = "_int8a>=46";
-    handle(str0, Dfstate::Initial, res);
-    handle(str1, Dfstate::Initial, res);
-    handle(str2, Dfstate::Initial, res);
-    handle(str3, Dfstate::Initial, res);
+    //std::string str4 = "in arg b = 23 =b9";
+    std::string str5 = "2+3   * 5";
+    handle(str0, INITIAL, res);
+    handle(str1, INITIAL, res);
+    handle(str2, INITIAL, res);
+    handle(str3, INITIAL, res);
+    //handle(str4, INITIAL, res);
+    handle(str5, INITIAL, res);
 
     for (std::vector<Token>::iterator iter = res.begin(); iter != res.end(); iter++)
         std::cout << type2str(iter->type) << ": " << iter->val << std::endl;
